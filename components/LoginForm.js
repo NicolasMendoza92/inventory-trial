c0300ae88e23343b84870fbe1d906949096ae14b
NicoMen <nicomendoza.92@gmail.com>
2024-02-12 18:04:30 +0100

    eye in password

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -14,26 +14,40 @@ export default function LoginForm() {
 
     const router = useRouter();
 
+    let flag = true
+    const showPass = () => {
+        if (flag) {
+            document.getElementById("password").type = "text";
+            document.getElementById("pass-icon").src = "/icons/eye.png"
+            flag = false;
+        } else {
+            document.getElementById("password").type = "password";
+            document.getElementById("pass-icon").src = "/icons/eye-slash.png"
+
+            flag = true;
+        }
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
+
         try {
-          const res = await signIn("credentials", {
-            email,
-            password,
-            redirect: false,
-          });
-    
-          if (res.error) {
-            setError("Invalid Credentials");
-            return;
-          }
-    
-          router.push("/inventary");
+            const res = await signIn("credentials", {
+                email,
+                password,
+                redirect: false,
+            });
+
+            if (res.error) {
+                setError("Invalid Credentials");
+                return;
+            }
+
+            router.push("/inventary");
         } catch (error) {
-          console.log(error);
+            console.log(error);
         }
-      };
+    };
 
     return (
         <div className="grid place-items-center h-screen">
@@ -42,17 +56,26 @@ export default function LoginForm() {
 
                 <form onSubmit={handleSubmit} className="flex flex-col gap-3">
                     <input
-                    className="px-2 py-2"
+                        className="px-2 py-2"
                         onChange={(e) => setEmail(e.target.value)}
                         type="text"
                         placeholder="Email"
                     />
-                    <input
-                    className="px-2 py-2"
-                        onChange={(e) => setPassword(e.target.value)}
-                        type="password"
-                        placeholder="Password"
-                    />
+                    <div className="flex inline-flex items-center">
+                        <input
+                            className="px-2 py-2"
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="password"
+                            id="password"
+                            placeholder="Password"
+                        />
+                        <img src={'/icons/eye-slash.png'} 
+                        id="pass-icon" 
+                        className="flex ms-1 w-8 cursor-pointer " 
+                        onClick={showPass} />
+                    </div>
+                    
+
                     <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
                         Login
                     </button>
