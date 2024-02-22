da4794179f327f495c11eb2cd8974418bd25c709
NicoMen <nicomendoza.92@gmail.com>
2024-02-22 13:50:16 +0100

    cambio en links de [id]

--- pages/operation/edit/[...id].js
+++ pages/operation/edit/[...id].js
@@ -107,12 +107,10 @@ export default function EditOperationPage() {
               <p> Edit Operation </p>
             </div>
             <div className='flex gap-2'>
-              <button className="bg-gray-300 text-white px-3 py-1 ms-1 mt-1 rounded shadow-sm hover:bg-gray-200" >
-                <Link href={goBack}>
+              <button onClick={goBack} className="bg-gray-300 text-white px-3 py-1 ms-1 mt-1 rounded shadow-sm hover:bg-gray-200" >
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
                   </svg>
-                </Link>
               </button>
               <button onClick={() => deleteOperation(id)} className="bg-red-300 text-white px-3 py-1 ms-1 mt-1 rounded shadow-sm hover:bg-red-200" >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
--- pages/projects/edit/[...id].js
+++ pages/projects/edit/[...id].js
@@ -70,12 +70,10 @@ export default function EditProjectPage() {
                     </svg>
                   </Link>
                 </button>
-                <button className="bg-gray-300 text-white px-3 py-1 ms-1 mt-1 rounded shadow-sm hover:bg-gray-200" >
-                  <Link href={goBack}>
+                <button onClick={goBack} className="bg-gray-300 text-white px-3 py-1 ms-1 mt-1 rounded shadow-sm hover:bg-gray-200" >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
                     </svg>
-                  </Link>
                 </button>
               </>
               }
